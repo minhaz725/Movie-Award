4a2481104c225a3b3ae757a9268a01ab79f75fe4
Minhaz <minhaz725@gmail.com>
2021-05-06 11:52:27 +0600

    task 1 & 2 done

---
+++ src/app/nominate/nominate.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'nominate',
+  templateUrl: './nominate.component.html',
+  styleUrls: ['./nominate.component.css']
+})
+export class NominateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+// tslint:disable-next-line:component-class-suffix
+export class SearchComponent {
+  baseuri = 'http://www.omdbapi.com/?s=';
+  induri = 'http://www.omdbapi.com/?t=';
+  val = 'x-men';
+  filter = '&movies';
+  apikey = '&apikey=98027472';
+  results;
+  showresult: boolean;
+  constructor(public http: HttpClient) {
+    http.get( this.baseuri + this.val + this.filter + this.apikey)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+  // tslint:disable-next-line:typedef
+  getSearchVal(val)
+  {
+    this.val = val;
+    console.warn(val);
+  }
+
+  // tslint:disable-next-line:typedef
+  getResult()
+  {
+    this.http.get(this.baseuri + this.val + this.filter + this.apikey)
+      .subscribe(response => {
+        this.results = response;
+        console.log(this.results);
+        this.showresult = true;
+      });
+
+  }
+}
